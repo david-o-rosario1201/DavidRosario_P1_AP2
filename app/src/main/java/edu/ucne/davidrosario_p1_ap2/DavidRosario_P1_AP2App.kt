2d8a7eda122bca8c63c9f481de85bf77425f3c0a
david-o-rosario1201 <finnnn023@gmail.com>
2024-09-18 20:06:25 -0400

    Agrega Hilt

---
+++ app/src/main/java/edu/ucne/davidrosario_p1_ap2/DavidRosario_P1_AP2App.kt
@@ -0,0 +1,7 @@
+package edu.ucne.davidrosario_p1_ap2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DavidRosario_P1_AP2App: Application()
--- app/src/main/java/edu/ucne/davidrosario_p1_ap2/MainActivity.kt
+++ app/src/main/java/edu/ucne/davidrosario_p1_ap2/MainActivity.kt
@@ -11,8 +11,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.davidrosario_p1_ap2.ui.theme.DavidRosario_P1_AP2Theme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
