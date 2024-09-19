e9bb8bab10a1d34e0b9975a879344d8cbae50ef3
david-o-rosario1201 <finnnn023@gmail.com>
2024-09-18 21:07:06 -0400

    Agrega el dao

---
+++ app/src/main/java/edu/ucne/davidrosario_p1_ap2/data/local/dao/AlgoDao.kt
@@ -0,0 +1,25 @@
+package edu.ucne.davidrosario_p1_ap2.data.local.dao
+
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.davidrosario_p1_ap2.data.local.AlgoEntity
+import kotlinx.coroutines.flow.Flow
+
+interface AlgoDao {
+    @Upsert
+    suspend fun save(Algo: AlgoEntity)
+
+    @Query("""
+        SELECT * FROM ALGOS
+        WHERE id = :id
+        LIMIT 1
+    """)
+    suspend fun find(id: Int): AlgoEntity?
+
+    @Delete
+    suspend fun delete(Algo: AlgoEntity)
+
+    @Query("SELECT * FROM ALGOS")
+    fun getAll(): Flow<List<AlgoEntity>>
+}
