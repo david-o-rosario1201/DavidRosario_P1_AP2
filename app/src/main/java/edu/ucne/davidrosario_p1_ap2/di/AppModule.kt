ae40a642667dcc0888080c65de8675672e1f3e8f
david-o-rosario1201 <finnnn023@gmail.com>
2024-09-18 21:06:43 -0400

    Agrega el AppModule con Room

---
+++ app/src/main/java/edu/ucne/davidrosario_p1_ap2/di/AppModule.kt
@@ -0,0 +1,29 @@
+package edu.ucne.davidrosario_p1_ap2.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.davidrosario_p1_ap2.data.local.database.Parcial1Db
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideParcial1Db(@ApplicationContext appContext: Context) =
+        Room.databaseBuilder(
+            appContext,
+            Parcial1Db::class.java,
+            "Parcial1.db"
+        ).fallbackToDestructiveMigration()
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideAlgoDao(parcial1Db: Parcial1Db) = parcial1Db.algoDao()
+}
