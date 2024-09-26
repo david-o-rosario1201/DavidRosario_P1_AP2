d9ccc34a1183cac6c4fbaa50ac02e30910490424
david-o-rosario1201 <finnnn023@gmail.com>
2024-09-25 21:44:38 -0400

    Creación de VentaUiState

---
+++ app/src/main/java/edu/ucne/davidrosario_p1_ap2/presentation/venta/VentaUiState.kt
@@ -0,0 +1,19 @@
+package edu.ucne.davidrosario_p1_ap2.presentation.venta
+
+import edu.ucne.davidrosario_p1_ap2.data.local.entities.VentaEntity
+
+data class VentaUiState(
+    val ventaId: Int? = null,
+    val cliente: String = "",
+    val galones: String = "",
+    val descuentoGalon: String = "",
+    val precio: String = "",
+    val totalDescontado: String = "",
+    val total: String = "",
+    val ventas: List<VentaEntity> = emptyList(),
+    val errorCliente: String? = "",
+    val errorGalones: String? = "",
+    val errorDescuentoGalon: String? = "",
+    val errorPrecio: String? = "",
+    val success: Boolean = false
+)
