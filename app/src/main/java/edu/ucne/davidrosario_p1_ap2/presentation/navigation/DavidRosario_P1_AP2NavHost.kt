fdab3ef46acdaaafec9cc69e54e428413021d035
david-o-rosario1201 <finnnn023@gmail.com>
2024-09-28 22:21:22 -0400

    Borrando imports que no estoy utilizando.

--- app/src/main/java/edu/ucne/davidrosario_p1_ap2/presentation/navigation/DavidRosario_P1_AP2NavHost.kt
+++ app/src/main/java/edu/ucne/davidrosario_p1_ap2/presentation/navigation/DavidRosario_P1_AP2NavHost.kt
@@ -1,8 +1,5 @@
 package edu.ucne.davidrosario_p1_ap2.presentation.navigation
 
-import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
