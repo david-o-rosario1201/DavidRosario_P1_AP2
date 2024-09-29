46edfde14d44dab267420fe67ee246e3af27f39f
david-o-rosario1201 <finnnn023@gmail.com>
2024-09-28 22:12:52 -0400

    Borrar el scaffold, la función Greeting y GreetingPreview

--- app/src/main/java/edu/ucne/davidrosario_p1_ap2/MainActivity.kt
+++ app/src/main/java/edu/ucne/davidrosario_p1_ap2/MainActivity.kt
@@ -4,13 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.davidrosario_p1_ap2.presentation.navigation.DavidRosario_P1_AP2NavHost
@@ -23,31 +16,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DavidRosario_P1_AP2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                    val navHostController = rememberNavController()
-                    DavidRosario_P1_AP2NavHost(navHostController = navHostController)
-                }
+                val navHostController = rememberNavController()
+                DavidRosario_P1_AP2NavHost(navHostController = navHostController)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DavidRosario_P1_AP2Theme {
-        Greeting("Android")
     }
 }
