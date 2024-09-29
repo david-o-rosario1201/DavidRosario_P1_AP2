d66c4ed39bb50e439a9df6e59b76a62a874f7a76
david-o-rosario1201 <finnnn023@gmail.com>
2024-09-28 22:15:06 -0400

    Se cambió el modo de borrar, ahora en lugar de pulsar un botón, es haciendo swipe, también se agregó la función VentaListScreenPreview y se mejoró la vista de la información en el card.

--- app/src/main/java/edu/ucne/davidrosario_p1_ap2/presentation/venta/VentaListScreen.kt
+++ app/src/main/java/edu/ucne/davidrosario_p1_ap2/presentation/venta/VentaListScreen.kt
@@ -1,35 +1,54 @@
+@file:OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
+
 package edu.ucne.davidrosario_p1_ap2.presentation.venta
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SwipeToDismissBox
+import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.compose.rememberNavController
 import edu.ucne.davidrosario_p1_ap2.data.local.entities.VentaEntity
+import edu.ucne.davidrosario_p1_ap2.presentation.navigation.Screen
+import edu.ucne.davidrosario_p1_ap2.ui.theme.DavidRosario_P1_AP2Theme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun VentaListScreen(
@@ -40,12 +59,11 @@ fun VentaListScreen(
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
     VentaListBodyScreen(
         uiState = uiState,
-        onEvent = viewModel::onEvent,
         onVentaClick = onVentaClick,
         onVentaAdd = onVentaAdd,
         onVentaDelete = { ventaId ->
             viewModel.onEvent(
-                VentaUiEvent.selectedVenta(ventaId)
+                VentaUiEvent.ventaIdChanged(ventaId)
             )
             viewModel.onEvent(
                 VentaUiEvent.Delete
@@ -58,11 +76,17 @@ fun VentaListScreen(
 @Composable
 fun VentaListBodyScreen(
     uiState: VentaUiState,
-    onEvent: (VentaUiEvent) -> Unit,
     onVentaClick: (Int) -> Unit,
     onVentaAdd: () -> Unit,
     onVentaDelete: (Int) -> Unit
 ){
+    val ventas = remember { mutableStateListOf(*uiState.ventas.toTypedArray()) }
+
+    LaunchedEffect(uiState.ventas) {
+        ventas.clear()
+        ventas.addAll(uiState.ventas)
+    }
+
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
@@ -92,7 +116,7 @@ fun VentaListBodyScreen(
                 .padding(innerPadding)
         ) {
             LazyColumn {
-                if(uiState.ventas.isEmpty()){
+                if(ventas.isEmpty()){
                     item {
                         Column(
                             modifier = Modifier
@@ -108,11 +132,15 @@ fun VentaListBodyScreen(
                     }
                 }
                 else{
-                    items(uiState.ventas){
+                    items(
+                        items = ventas,
+                        key = { it.ventaId ?: 0}
+                    ){
                         VentaRow(
                             it = it,
                             onVentaClick = onVentaClick,
-                            onVentaDelete = onVentaDelete
+                            onVentaDelete = onVentaDelete,
+                            modifier = Modifier.animateItemPlacement(tween(200))
                         )
                     }
                 }
@@ -125,41 +153,148 @@ fun VentaListBodyScreen(
 fun VentaRow(
     it: VentaEntity,
     onVentaClick: (Int) -> Unit,
-    onVentaDelete: (Int) -> Unit
+    onVentaDelete: (Int) -> Unit,
+    modifier: Modifier = Modifier
 ){
-    Card(
-        onClick = {
-            onVentaClick(it.ventaId ?: 0)
-        },
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(top = 20.dp)
-            .padding(horizontal = 10.dp)
-            .heightIn(160.dp)
-    ){
-        Row {
-            Column {
-                Text(it.cliente)
-                Text(it.galones.toString())
-                Text(it.descuentoGalon.toString())
-                Text(it.precio.toString())
-                Text(it.totalDescontado.toString())
-                Text(it.total.toString())
-            }
-
-            IconButton(
-                onClick = {
+    val scope = rememberCoroutineScope()
+    val swipeToDismissState = rememberSwipeToDismissBoxState(
+        confirmValueChange = { state ->
+            if(state == SwipeToDismissBoxValue.EndToStart){
+                scope.launch {
+                    delay(500)
                     onVentaDelete(it.ventaId ?: 0)
-                },
-                modifier = Modifier
-                    .padding(8.dp)
-                    .align(Alignment.CenterVertically)
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Delete,
-                    contentDescription = "Eliminar Venta"
-                )
+                }
+                true
+            } else{
+                false
             }
         }
+    )
+
+    SwipeToDismissBox(
+        state = swipeToDismissState,
+        backgroundContent = {
+            val backgroundColor by animateColorAsState(
+                targetValue = when (swipeToDismissState.currentValue) {
+                    SwipeToDismissBoxValue.StartToEnd -> Color.Green
+                    SwipeToDismissBoxValue.EndToStart -> Color.Red
+                    SwipeToDismissBoxValue.Settled -> Color.White
+                }, label = ""
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(backgroundColor)
+            )
+        },
+        modifier = modifier
+    ) {
+        Card(
+            onClick = {
+                onVentaClick(it.ventaId ?: 0)
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 20.dp)
+                .padding(horizontal = 10.dp)
+                .height(160.dp)
+        ){
+            Column(
+                modifier = Modifier
+                    .padding(10.dp)
+            ){
+                Column(
+                    modifier = Modifier
+                ){
+                    Row(
+                        modifier = Modifier
+                            .padding(horizontal = 10.dp)
+                    ){
+                        Text(
+                            text = "Cliente: ",
+                            style = MaterialTheme.typography.bodyMedium,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(it.cliente)
+                    }
+                    Row(
+                        modifier = Modifier
+                            .padding(horizontal = 10.dp)
+                    ){
+                        Text(
+                            text = "Galones: ",
+                            style = MaterialTheme.typography.bodyMedium,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(it.galones.toString())
+                    }
+                    Row(
+                        modifier = Modifier
+                            .padding(horizontal = 10.dp)
+                    ){
+                        Text(
+                            text = "Descuento por Galón: ",
+                            style = MaterialTheme.typography.bodyMedium,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(it.descuentoGalon.toString())
+                    }
+                }
+
+                Column {
+                    Row(
+                        modifier = Modifier
+                            .padding(horizontal = 10.dp)
+                    ){
+                        Text(
+                            text = "Precio: ",
+                            style = MaterialTheme.typography.bodyMedium,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(it.precio.toString())
+                    }
+
+                    Row(
+                        modifier = Modifier
+                            .padding(horizontal = 10.dp)
+                    ){
+                        Text(
+                            text = "Total Descontado: ",
+                            style = MaterialTheme.typography.bodyMedium,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(it.totalDescontado.toString())
+                    }
+
+                    Row(
+                        modifier = Modifier
+                            .padding(horizontal = 10.dp)
+                    ){
+                        Text(
+                            text = "Total: ",
+                            style = MaterialTheme.typography.bodyMedium,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(it.total.toString())
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun VentaListScreenPreview() {
+    DavidRosario_P1_AP2Theme {
+        val navHostController = rememberNavController()
+        VentaListScreen(
+            onVentaClick = { ventaId ->
+                navHostController.navigate(Screen.VentaScreen(ventaId))
+            },
+            onVentaAdd = {
+                navHostController.navigate(Screen.VentaScreen(0))
+            }
+        )
     }
 }
